3c88be7b9846cfc9e24db5af3788cbf884b3c0e0
Marko Marinkovic <marko.marinkovic@study.hs-duesseldorf.de>
2023-11-26 14:35:02 +0100

    added the directory fairsharer1 to repository fairsharer

---
+++ fairsharer1/fairsharer/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about fairsharer"""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Marko Marinkovic"
+__email__ = "marko.marinkovic@study.hs-duesseldorf.de"
+__version__ = "0.1.0"
